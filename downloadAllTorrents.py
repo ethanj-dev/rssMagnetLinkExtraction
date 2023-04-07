31e70093fbbc3d56df9d91c37be8916ec4d347e9
Jinu Jang <wjdqkf1@gmail.com>
2023-04-07 11:28:38 +0900

    Update downloadAllTorrents.py
    updated

--- downloadAllTorrents.py
+++ downloadAllTorrents.py
@@ -1,2 +1,38 @@
 import os
-import 
+from pathlib import Path
+from bs4 import BeautifulSoup
+import requests as re
+from shutil import move
+
+rootUrl = "https://nyaa.si"
+urlList = ["/?f=0&c=0_0&q=erai"]
+maxPageLen = 9
+originName = ""
+saveDir = os.path.join(os.getcwd(), "torrent_files")
+
+Path(saveDir).mkdir(parents=True, exist_ok=True)
+
+for urls in urlList:
+    for pages in range(maxPageLen):
+        url = rootUrl + urls + "&p={}".format(pages+1)
+        response = re.get(url)
+
+        soup = BeautifulSoup(response.content, "html.parser")
+
+        for link in soup.find_all("a"):
+            href = link.get("href")
+
+            if href and "/view/" in href:
+                if link.get("title") is not None:
+                    originName = link.get("title")
+
+            if href and "/download/" in href:
+                filename = os.path.join(saveDir, href.split("/")[-1])
+                response = re.get(rootUrl + href)
+
+                print("Downloading filename : {} - {}".format(originName, filename))
+                with open(filename, "wb") as f:
+                    f.write(response.content)
+
+                renameFile = originName + ".torrent"
+                move(filename, os.path.join(saveDir, renameFile))
